2955ef3ee40e226f8b03d099f4a33afae4b92cc6
LilAliTech <lilalix20@gmail.com>
2024-01-08 20:14:19 +0000

    Add NextAuth + UserAvatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Logo from './Logo'
 import { DarkModeToggle } from './DarkModeToggle'
 import UserButton from './UserButton'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/auth'
+import Link from 'next/link'
+import { MessagesSquareIcon } from 'lucide-react'
 
-function Header() {
+async function Header() {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
         <nav className=' flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
@@ -12,12 +18,20 @@ function Header() {
             <div className='flex-1 flex items-center justify-end space-x-4'>
               {/* LanguageSelect */}
 
-              {/* Session && (
-                ...
-              ) */}
+              {session ? (
+                <>
+                  <Link
+                    href={'/chat'} prefetch={false}
+                  >
+                    <MessagesSquareIcon className='text-black dark:text-white' />
+                  </Link>
+                </>
+              ): (
+                <Link href='/pricing'>Pricing</Link>
+              )}
 
               <DarkModeToggle />
-              <UserButton />
+              <UserButton session={session} />
             </div>
         </nav>
         {/* Upgrade Banner */}
--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,13 +8,24 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import UserAvatar from "./UserAvatar"
+import { Session } from "next-auth"
+import { Button } from "./ui/button"
+import { signIn } from "next-auth/react"
 
 
-function UserButton() {
+function UserButton({ session }: {session: Session | null}) {
+
+  if(!session) return (
+    <Button variant={'outline'} onClick={() => signIn()}>
+      Sign In
+    </Button>
+  )
+
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar name="Muhammed" image="https://github.com/shadcn.png" />
+        <UserAvatar name={session.user?.name} image={session.user?.image} />
       </DropdownMenuTrigger>
     <DropdownMenuContent>
       <DropdownMenuLabel>My Account</DropdownMenuLabel>
