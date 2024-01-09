44dd3a0ea172c17fe0c437d69de40f23aa7c6320
LilAliTech <lilalix20@gmail.com>
2024-01-09 17:49:31 +0000

    Add Hero section to landing page

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -2,11 +2,19 @@
 
 import { MessageSquarePlusIcon } from "lucide-react"
 import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
 
 
 function CreateChatButton() {
+    const router = useRouter();
+
+    const createNewChat = async() => {
+        //all the logic here....
+        router.push(`/chat/abc`);
+    }
+
   return (
-    <Button variant={'ghost'}>
+    <Button onClick={createNewChat} variant={'ghost'}>
         <MessageSquarePlusIcon/>
     </Button>
   )
